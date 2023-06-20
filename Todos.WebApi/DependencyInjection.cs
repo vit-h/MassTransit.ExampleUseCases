3000644e7b256a77eb9809b71912e6bae2b5e961
vit-h <vitalii.hon@gmail.com>
2023-06-20 13:00:19 -0500

    add notes

--- Todos.WebApi/DependencyInjection.cs
+++ Todos.WebApi/DependencyInjection.cs
@@ -1,4 +1,5 @@
 ﻿using MassTransit;
+using RabbitMQ.Client;
 using System.Reflection;
 
 namespace Todos.WebApi;
@@ -27,6 +28,29 @@ public static class DependencyInjection
                 });
 
                 cfg.ConfigureEndpoints(context);
+
+
+                // If need to receive messages to multiple instances:
+                // Create a unique queue name for each instance and map to the same consumer
+                // For example to get duplicated messages to all replicated instances:
+                //var uniquePrefix = Environment.MachineName.ToLower();
+                //var queueName = $"some-name-for-lookup-{uniquePrefix}";
+                //cfg.ReceiveEndpoint(queueName, e => e.Consumer<UpdatesConsumer>(context));
+
+                // If need to Delete the queue when the application closes
+                //AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+                //{
+                //    var connectionFactory = new ConnectionFactory()
+                //    {
+                //        HostName = rabbitMqConfig["Host"],
+                //        UserName = rabbitMqConfig["Username"],
+                //        Password = rabbitMqConfig["Password"]
+                //    };
+                //    using var connection = connectionFactory.CreateConnection();
+                //    using var channel = connection.CreateModel();
+                //    channel.QueueDelete(queueName);
+                //    channel.ExchangeDelete(queueName);
+                //};
             });
         });
 }
